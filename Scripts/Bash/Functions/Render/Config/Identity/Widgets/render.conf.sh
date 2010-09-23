43eb806ea556f1858a72eb5b268abdc1b6ec0ef2
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-09-23 13:49:27 +0000

    Update Widgets pre-rendering configuration script to add icon-admonition-doc-old and icon-admonition-doc-redirect to widgets matching list configuration.

--- Scripts/Bash/Functions/Render/Config/Identity/Widgets/render.conf.sh
+++ Scripts/Bash/Functions/Render/Config/Identity/Widgets/render.conf.sh
@@ -51,5 +51,7 @@ function render_loadConfig {
     icon-admonition-star.svg:       icon-admonition-star.sed
     icon-admonition-svn.svg:        icon-admonition-svn.sed
     icon-admonition-idea.svg:       icon-admonition-idea.sed
+    icon-admonition-doc-old.svg:      icon-admonition-doc-old.sed
+    icon-admonition-doc-redirect.svg: icon-admonition-doc-redirect.sed
     "
 }
