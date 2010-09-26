3515e16c93f682f594bf3a4934013b617097d47d
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-09-26 08:35:55 +0000

    Remove old structure from TreeFlower's KDM directory.

--- Identity/Themes/Motifs/TreeFlower/Distro/BootUp/KDM/tgz/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Syntax: ./render.sh [VERSION [RESOLUTION]]
-#--------------------------------------
-~/artwork/trunk/Scripts/Bash/Config/Identity/Themes/Distro/BootUp/KDM/build-targz.sh "$1" "$2"
-#--------------------------------------
