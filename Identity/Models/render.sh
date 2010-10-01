01db442c1ab50541b19a21bb36c694c205a09e03
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-10-01 04:37:53 +0000

    Update trunk/Identity/Models.

--- Identity/Models/render.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Syntax: ./render.conf.sh
-#--------------------------------------
-~/artwork/trunk/Scripts/Bash/Config/Identity/Models/render.conf.sh "$1"
-#--------------------------------------
