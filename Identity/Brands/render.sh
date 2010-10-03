49bca69b4c818f60d423e7eef471283307f9916d
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-10-03 18:55:43 +0000

    Inside trunk/Identity/Brands:
        - Rename Palettes dirctory to Colors.
        - Remove render.sh invokcation scripts. It is obsolete now by
          centos-art.sh script.

--- Identity/Brands/render.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Syntax: ./render.sh [REGEX]
-#--------------------------------------
-~/artwork/trunk/Scripts/Bash/Config/Identity/Brands/render.conf.sh "$1"
-#--------------------------------------
