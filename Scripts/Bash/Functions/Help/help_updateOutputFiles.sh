312c7efbfd51d32febd073baa29a639142e751a1
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-11-05 09:05:07 +0000

    Update help_updateOutputFiles.sh.
        - Before this commit, many different source directories were
          specified for be appened when importing images. This
          configuration restricted the amount of image files to include
          based on those different source directories only.
    
        - In this commit the restriction was removed using just
          /home/centos/artwork as source directory for image inclusion. In
          this configuration image path to use inside texinfo files, must
          be based on repository base directory structure, starting from
          trunk/ up to image destination inside repository. Practically,
          you can include whatever image you want without limitation using
          this configuration.

--- Scripts/Bash/Functions/Help/help_updateOutputFiles.sh
+++ Scripts/Bash/Functions/Help/help_updateOutputFiles.sh
@@ -80,12 +80,7 @@ function help_updateOutputFiles {
     texi2html ${MANUALS_FILE[1]} --output=${MANUALS_DIR[4]} --split section \
         --nosec-nav \
         --css-include=/home/centos/artwork/trunk/Identity/Models/Css/Texi2html/stylesheet.css \
-        --I=/home/centos/artwork/trunk/Identity/Fonts/Img/ \
-        --I=/home/centos/artwork/trunk/Identity/Models/Img/Scripts/Png/ \
-        --I=/home/centos/artwork/trunk/Identity/Models/Img/Corporate/Png/ \
-        --I=/home/centos/artwork/trunk/Identity/Models/Img/Promo/Web/Png/ \
-        --I=/home/centos/artwork/trunk/Identity/Models/Img/Promo/Web/Concepts/Png/ \
-        --I=/home/centos/artwork/trunk/Identity/Models/Img/Distro/Png/
+        --I=/home/centos/artwork/
 
     # Apply html transformations.
     sed -r -i \
