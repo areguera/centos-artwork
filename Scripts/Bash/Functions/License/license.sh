fa105b79fcdb422c72888bc747bf3d637352a5d4
Alain Reguera Delgado <alain.reguera@gmail.com>
2010-11-25 18:13:25 +0000

    Update license functionality to reuse centos-art.sh personal information, defined in cli function file.

--- Scripts/Bash/Functions/License/license.sh
+++ Scripts/Bash/Functions/License/license.sh
@@ -25,34 +25,23 @@
 
 function license {
 
+    local CLILICENSE=''
+
     # Define command-line information.
-    local COMMAND="centos-art.sh"
-    local DESCRIPTION="$COMMAND - `gettext "Automate frequent tasks inside CentOS Artwork Repository."`"
-    local RELEASE="alpha"
-    local COPYRIGHT="Copyright (C) 2009-2010 Alain Reguera Delgado."
-    local LICENSE="This program is free software; you can redistribute
-        it and/or modify it under the terms of the GNU General Public License
-        as published by the Free Software Foundation; either version 2 of the
-        License, or (at your option) any later version.
-     
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-        General Public License for more details.
-         
-        You should have received a copy of the GNU General Public License
-        along with this program; if not, write to the Free Software
-        Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-        USA."
+    CLILICENSE="This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA."
 
     # Build license message.
-    local LICENSE="$DESCRIPTION
+    CLILICENSE="$CLINAME ($CLIVERSION) $CLIDESCRIP
 
-        $COPYRIGHT
+        $CLICOPYRIGHT
 
-        $LICENSE"
+        $CLILICENSE"
 
     # Output license message.
-    cli_printMessage "$LICENSE" "AsHeadingLine"
+    cli_printMessage "$CLILICENSE" "AsHeadingLine"
 
 }
