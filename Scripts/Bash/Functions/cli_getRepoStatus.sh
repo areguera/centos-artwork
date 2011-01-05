a9facac279abb081904bd7580258aa8d81be415e
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-01-05 03:19:45 +0000

    Update cli_getRepoStatus.sh

--- Scripts/Bash/Functions/cli_getRepoStatus.sh
+++ Scripts/Bash/Functions/cli_getRepoStatus.sh
@@ -6,7 +6,7 @@
 # specified. Use this function to perform verifications based a
 # repository LOCATION status. 
 #
-# Copyright (C) 2009-2011  Alain Reguera Delgado
+# Copyright (C) 2009-2011 Alain Reguera Delgado
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
