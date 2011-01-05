9a3ef2d1bd514fa249d25b6e440a3fd9363b780d
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-01-05 03:19:47 +0000

    Update cli_getTemporalFile.sh

--- Scripts/Bash/Functions/cli_getTemporalFile.sh
+++ Scripts/Bash/Functions/cli_getTemporalFile.sh
@@ -4,7 +4,7 @@
 # temporal file. Use this function whenever you need to create
 # temporal files inside centos-art.sh script.
 #
-# Copyright (C) 2009-2011  Alain Reguera Delgado
+# Copyright (C) 2009-2011 Alain Reguera Delgado
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
