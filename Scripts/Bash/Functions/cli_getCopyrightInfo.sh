606e4cdc8d928a909d61c29d10c70df40bf3de69
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-01-20 14:57:36 +0000

    Update cli_getCopyrightInfo.sh.

--- Scripts/Bash/Functions/cli_getCopyrightInfo.sh
+++ Scripts/Bash/Functions/cli_getCopyrightInfo.sh
@@ -43,46 +43,60 @@
 #      over all images inside one CentOS theme, the copyright
 #      information of CentOS themes is printed only on images related
 #      to Anaconda progress first slides.
+#
+#      The copyright information printed on Anaconda progress first
+#      slides is used to print the creation rights of The CentOS
+#      Project over its creation (i.e., The CentOS Distribution) not
+#      The CentOS Theme itself.
 # 
-# At this point, we've defined who own the creation rights of artistic
-# motifs, design models, and the CentOS themes.
+# At this point, we've defined who own the creation rights of image
+# components. Now, it is important to remark that if we need to show
+# one creation copyright, in different places, all references to the
+# same cration copyright information should be the same. Cannot be any
+# ambiguity among them. The best way to reach this is having just one
+# unique definition, and build images using that unique copyright
+# definition as reference.
 #     
-# Now, in order for the CentOS Community to use these components, it
-# is required that each component author relases his/her creation
-# under a license that grants the freedom of using, studying, changing
-# and releasing improved versions of his/her creation. Also, the license
-# should prevent any privatizing practice or any kind of darkness that
-# put in risk the freedom of CentOS Community.
+# Another relevant point, we need to be aware of, is that related to
+# the licenses. As creators has the creation rights, they have the
+# right to distribute their work as their consideration.  If creators
+# release their creations under the terms of a privatizing license,
+# that creation will be almost useless for The CentOS Community. So,
+# in order for creations to be useful in The CentOS Community,
+# creators should distribution their creations under the terms of a
+# license that grants the freedom of using, studying, changing and
+# releasing improved versions of them. Likewise, the license should
+# prevent any privatizing practice or any kind of darkness that put in
+# risk the freedom of The CentOS Community.
 # 
-# What license to use is a decision that CentOS Community has to take.
-# The license adopted, by CentOS Community, specifies the ethical terms
-# the CentOS Community agree to work with. It reflects the
-# philosophical thinking of CentOS Community, so all creation inside
-# CentOS Community should be released under such license terms, no
-# matter who the person who creates something be. If some person want
-# to create for CentOS, that person has to aggree with CentOS
-# Community philosophical thinking (i.e. that reflected by license
-# adopted to release creations). This way, that's because it is so
-# important that the decision of what license to use be a collective
-# decision, that everyone understand why we are using this license and
-# not other.
+# What license should be used to distribute images (and the components
+# used to build them) is a decision for The CentOS Community to take.
+# The license adopted, by The CentOS Community, specifies the ethical
+# terms The CentOS Community is agree to work with. It is a reference
+# we all have to agree with and follow in the sake of our community
+# and ourselves freedom.  That's why license selection has to be a
+# collective decision, and also be constantly in question.
 #
 # I'm not a lawyer. So, I can't give details of something I don't know
-# certainly.  All I can do is using my intuition and it says to me
-# that Creative Common Attribution-ShareAlike 3.0 License
-# (http://creativecommons.org/licenses/by-sa/3.0/) seems to be a good
-# candidate. It has been used in the wiki (http://wiki.centos.org/)
-# for some time. Also, other relevant projects like The Tango Project
-# are using it to release art works and similar creations.
+# certainly.  All I've done so far is using my intuition and basic
+# understanding of what copyright and license is. So, if you have a
+# deeper understanding and experience in such legal topics, please
+# feel free to make your own revisions to ideas described here.
 #         
-# So, in the sake of covering copyright information and license needs
+# In the sake of covering copyright information and license needs
 # inside centos-art.sh script, all images produced by `centos-art.sh'
-# script will be released using the copyright information his/her
-# author (see above) and the license Creative Common
+# script will be released using the copyright information of his/her
+# creator (see above) and the license Creative Common
 # Attribution-ShareAlike 3.0 License
-# (http://creativecommons.org/licenses/by-sa/3.0/).  This could change
-# in the future, after a collective discussion in the CentOS mailing
-# list.
+# (http://creativecommons.org/licenses/by-sa/3.0/).  
+#
+# The Creative Common Attribution-ShareAlike 3.0 License has been
+# adopted in The CentOS Wiki (http://wiki.centos.org/) and seems to be
+# good as well to distribute image-specific creations under CentOS
+# Artwork Repository. Of course, as licenses are always questionable,
+# the one we've choosed could be changed in the future after a
+# collective discussion in the CentOS mailing list, in order to better
+# reflect the ethical feelings of our community.
 #
 # Copyright (C) 2009-2011 Alain Reguera Delgado
 # 
@@ -107,36 +121,43 @@
 
 function cli_getCopyrightInfo {
 
-    local NOTE=''
-    local DIR=''
     local -a DIRS
+    local -a FILES
     local -a NOTES
+    local NOTE=''
     local COUNT=0
 
     # Define directory structures that don't use default information.
     DIRS[0]="$(cli_getRepoTLDir)/Identity/Themes/Motifs/$(cli_getThemeName)/Concept"
+    DIRS[1]="$(cli_getRepoTLDir)/Identity/Themes/Motifs/$(cli_getThemeName)/Promo"
+
+    # Define absolute path to file from which we retrive copyright
+    # information for directory structures that don't use default
+    # information. 
+    FILES[0]="$(cli_getRepoTLDir)/Identity/Themes/Motifs/$(cli_getThemeName '--name')/copyright.txt"
 
     case "$1" in
 
         '--description' )
 
             # Define default description information.
-            NOTE="The CentOS Project corporate visual identity"
+            NOTE="The CentOS Project corporate visual identity."
 
             # Define description information for directory structures
             # that don't use default description information.
-            NOTES[0]="=NAME= is an artistic motif and theme for ${NOTE}."
+            NOTES[0]="=NAME= is an artistic motif and theme for ${NOTE}"
             ;;
 
         '--license' )
 
             # Define default license information used by all
             # image-based creations inside CentOS Artwork Repository.
-            NOTE="Creative Common Attribution-ShareAlike 3.0 License"
+            NOTE="Creative Common Attribution-ShareAlike 3.0 License."
 
             # Define license information for directory structures that
             # don't match default license information.
-            NOTES[0]="=NAME= artistic motif and theme are released under ${NOTE}."
+            NOTES[0]="=NAME= artistic motif and theme are released under ${NOTE}"
+            NOTES[1]="The CentOS distribution is released as GPL."
             ;;
 
         '--copyright' | * )
@@ -145,10 +166,10 @@ function cli_getCopyrightInfo {
             NOTE="Copyright © 2003-$(date +%Y) The CentOS Project. All rights reserved."
 
             # Define copyright information for directory structures
-            # that don't use default copyright information.
-            NOTES[0]=$(cli_readFileContent \
-                "$(cli_getRepoTLDir)/Identity/Themes/Motifs/$(cli_getThemeName)/authors.txt" \
-                '--copyright')
+            # that don't match default copyright information.
+            if [[ -f ${FILES[0]} ]];then
+                NOTES[0]=$(cli_readFileContent "${FILES[0]}" '--copyright')
+            fi
             ;;
     esac
 
@@ -157,13 +178,13 @@ function cli_getCopyrightInfo {
     while [[ ${COUNT} -lt ${#DIRS[*]} ]];do
         if [[ $ACTIONVAL =~ "${DIRS[$COUNT]}" ]];then
             if [[ "${NOTES[$COUNT]}" != '' ]];then
-                NOTE=${NOTES[$COUNT]}
+                NOTE="${NOTES[$COUNT]}"
             fi
         fi
         COUNT=$(($COUNT + 1))
     done
 
     # Print information.
-    echo $NOTE
+    echo "$NOTE"
 
 }
