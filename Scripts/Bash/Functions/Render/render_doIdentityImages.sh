891cdf0052c682fcfb36a5a9233966d2a605344f
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-02-20 05:46:58 +0000

    Unify render_getIdentityDefs into render_doIdentityImages. There is no longer other variations of rendition (like the text-based rendition we used to produce sed translation files) but image rendition. So unify into render_doIdentityImages and remove render_getIdentityDefs.

--- Scripts/Bash/Functions/Render/render_doIdentityImages.sh
+++ Scripts/Bash/Functions/Render/render_doIdentityImages.sh
@@ -43,8 +43,79 @@ function render_doIdentityImages {
     # approching :-).
     for FILE in $FILES; do
 
-        # Get common definitions for all identity rendition actions.
-        render_getIdentityDefs
+        # Define final location of translation file.
+        TRANSLATION=$(dirname $FILE \
+           | sed -r 's!/trunk/(Identity/)!/trunk/Locales/\1!')/$(cli_getCurrentLocale).po
+
+        # Print final location of translation file.
+        if [[ ! -f "$TRANSLATION" ]];then
+            cli_printMessage "`gettext "None"`" "AsTranslationLine"
+        else
+            cli_printMessage "$TRANSLATION" 'AsTranslationLine'
+        fi
+
+        # Define final location of template file.
+        TEMPLATE=${FILE}
+
+        # Print final location of template file.
+        if [[ ! -f "$TEMPLATE" ]];then
+            cli_printMessage "`gettext "None"`" "AsDesignLine"
+        else
+            cli_printMessage "$TEMPLATE" 'AsDesignLine'
+        fi
+ 
+        # Define final location of output directory.
+        render_getIdentityDirOutput
+
+        # Get relative path to file. The path string (stored in FILE)
+        # has two parts: 1. the variable path and 2. the common path.
+        # The variable path is before the common point in the path
+        # string. The common path is after the common point in the
+        # path string. The common point is the name of the parent
+        # directory (stored in PARENTDIR).
+        #
+        # trunk/Locales/Identity/.../Firstboot/3/splash-small.svg
+        # -------------------------^| the     |^------------^
+        # variable path             | common  |    common path
+        # -------------------------v| point   |    v------------v
+        # trunk/Identity/Themes/M.../Firstboot/Img/3/splash-small.png
+        #
+        # What we do here is remove the varibale path, the common
+        # point, and the file extension parts in the string holding
+        # the path retrived from design models directory structure.
+        # Then we use the common path as relative path to store the
+        # the final image file.
+        #
+        # The file extension is removed from the common path because
+        # it is set when we create the final image file. This
+        # configuration let us use different extensions for the same
+        # file name.
+        #
+        # When we render using renderImage function, the structure of
+        # files under the output directory will be the same used after
+        # the common point in the related design model directory
+        # structure.
+        FILE=$(echo ${FILE} \
+            | sed -r "s!.*${PARENTDIR}/!!" \
+            | sed -r "s/\.(svgz|svg)$//")
+
+        # Define absolute path to final file (without extension).
+        FILE=${OUTPUT}/$(basename "${FILE}")
+
+        # Define instance name from design model.
+        INSTANCE=$(cli_getTemporalFile ${TEMPLATE})
+
+        if [[ -f ${TRANSLATION} ]];then
+            # Create translated instance from design model.
+            /usr/bin/xml2po -p ${TRANSLATION} -o ${INSTANCE} ${TEMPLATE}
+        else
+            # Create non-translated instance form design model.
+            /bin/cat ${TEMPLATE} > ${INSTANCE}    
+        fi
+
+        # Apply replacement of translation markers to design model
+        # translated instance.
+        cli_replaceTMarkers ${INSTANCE}
 
         # Check export id inside design templates.
         grep "id=\"$EXPORTID\"" $INSTANCE > /dev/null
