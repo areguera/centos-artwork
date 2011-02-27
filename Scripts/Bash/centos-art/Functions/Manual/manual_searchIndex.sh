476079aaae2f97c0f72e28af76f8189414a9d4be
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-02-27 17:09:40 +0000

    Update manual_searchIndex.sh.

--- Scripts/Bash/centos-art/Functions/Manual/manual_searchIndex.sh
+++ Scripts/Bash/centos-art/Functions/Manual/manual_searchIndex.sh
@@ -26,6 +26,19 @@
 
 function manual_searchIndex {
 
+    # Check flag filter. By default flag filter has the `.+' value
+    # which is not very descriptive in the sake of an index-search.
+    # So, when no value is passed through --filter option use top node
+    # as default value for index-search.
+    if [[ "$FLAG_FILTER" == '.+' ]];then
+        cli_printMessage "`gettext "Use the \\\`--filter' option to define the search pattern."`" 'AsErrorLine'
+        cli_printMessage "$(caller)" 'AsToKnowMoreLine'
+    fi
+
+    # Print action message.
+    cli_printMessage "${MANUAL_BASEFILE}.info.bz2" 'AsReadingLine'
+
+    # Execute info command to perform an index-search.
     /usr/bin/info --index-search="$FLAG_FILTER" --file=${MANUAL_BASEFILE}.info.bz2
 
 }
