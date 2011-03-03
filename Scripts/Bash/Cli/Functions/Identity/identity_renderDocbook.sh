0690a809720bf90a29544315a3a215ebf95d6f1c
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-03-03 23:55:24 +0000

    Add identity_renderDocbook.sh.

---
+++ Scripts/Bash/Cli/Functions/Identity/identity_renderDocbook.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# identity_renderDocbook.sh -- This function performs base-rendition
+# action for DocBook files.
+#
+# Copyright (C) 2009-2011 Alain Reguera Delgado
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+# 
+# ----------------------------------------------------------------------
+# $Id$
+# ----------------------------------------------------------------------
+
+function identity_renderDocbook {
+
+    # Produce xhtml output from docbook template instance using XSL
+    # stylesheets as reference.
+    identity_renderDocbookXhtml
+
+    # Produce plaintext output from html outout.
+    identity_renderHtmlPlaintext
+
+}
