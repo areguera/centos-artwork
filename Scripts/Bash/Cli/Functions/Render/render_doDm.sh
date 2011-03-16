d1b3f57ce562365df9e237640a56dcc04279b689
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-03-16 02:05:11 +0000

    Update render_doDm.sh:
        - Start using symbol-resized-48.png.
    
        - In the sake of consistency, the inclusion of The CentOS Symbol
          and other types of branding possibly will be removed from
          scalable vector graphics and put into images using ImageMagick
          instead.
    
          We are in need of a functionality dedicated to brand images
          based on their dimensions, and purpose. Doing this inside
          Scalable vector graphics is quiet flexible and because such
          flexibility it would be difficult to use the exact symbol for
          all images, or worst undesired changes could be introduced to
          The CentOS Symbol which is not desired at all.
          
          Instead, we concentrate The CentOS Symbol in a single place
          (trunk/Identity/Brands) and using the functionality mentioned
          above print the very same symbol in all images that need one or
          something like that. This isn't implemented yet but mention it
          here to help myself remembering it :).

--- Scripts/Bash/Cli/Functions/Render/render_doDm.sh
+++ Scripts/Bash/Cli/Functions/Render/render_doDm.sh
@@ -82,7 +82,7 @@ function render_doDm {
     fi
 
     # Define source files using absolute paths.
-    SRC[0]=$(cli_getRepoTLDir)/Identity/Brands/Img/Symbol/48.png
+    SRC[0]=$(cli_getRepoTLDir)/Identity/Brands/Img/symbol-resized-48.png
     SRC[1]=${OUTPUT}/release.png
     SRC[2]=${OUTPUT}/screenshot.png
     SRC[3]=$(dirname $TEMPLATE)/GdmGreeterTheme.xml
