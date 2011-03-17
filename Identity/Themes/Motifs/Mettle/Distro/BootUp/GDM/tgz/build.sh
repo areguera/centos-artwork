628e3624de1ca14898e6690eaa5cb04207b52585
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-03-17 17:27:21 +0000

    Remove Mettle theme. It is too old, too ugly, and space consuming. Bisides, it is no use.

--- Identity/Themes/Motifs/Mettle/Distro/BootUp/GDM/tgz/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Syntax: ./render.sh [VERSION [RESOLUTION]]
-#--------------------------------------
-~/artwork/trunk/Scripts/Bash/Config/Identity/Themes/Distro/BootUp/GDM/build-targz.sh "$1" "$2"
-#--------------------------------------
