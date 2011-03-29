d91e3640e78b131cb82e3c8feb28223b5d0d6f53
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-03-29 01:37:03 +0000

    Update TEXTDOMAINDIR path in centos-art.sh.

--- Scripts/centos-art.sh
+++ Scripts/centos-art.sh
@@ -40,7 +40,7 @@ export CLI_BASEDIR="${HOME}/artwork/trunk/Scripts"
 # Initizalize internazionalization through gettext.
 . gettext.sh
 export TEXTDOMAIN=${CLI_PROGRAM}.sh
-export TEXTDOMAINDIR=${HOME}/artwork/trunk/Locales
+export TEXTDOMAINDIR=${CLI_BASEDIR}/Locales
 
 # Initialize common function scripts.
 FILES=$(ls ${CLI_BASEDIR}/Functions/{cli,cli_*}.sh)
