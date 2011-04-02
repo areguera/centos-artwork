fa0c7520fc7790d7c067b3a7fb91c8a5cd140772
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-04-02 20:00:47 +0000

    Add render_convertPngToThumbnail.sh.

---
+++ Scripts/Functions/Render/render_convertPngToThumbnail.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# render_convertPngToThumbnail.sh -- This function provides post-rendition to
+# create thumbnails from images produced by centos-art base-rendition.
+# Thumbnails are created in PNG and JPG format for you to decide which
+# is the more appropriate one. When no size is specified, thumbnails
+# are created at 250 pixels width and height is automatically
+# calculated to match the image ratio.
+#
+# Copyright (C) 2009-2011 Alain Reguera Delgado
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+# ----------------------------------------------------------------------
+# $Id$
+# ----------------------------------------------------------------------
+
+function render_convertPngToThumbnail {
+
+    # Get image size.
+    local SIZES=$(render_getConfigOption "$ACTION" '2-')
+
+    # Check base file existence.
+    cli_checkFiles "${FILE}.png" 'f'
+
+    # Check image sizes and do convertion.
+    if [[ "$SIZES" == "" ]];then
+        SIZES='250'
+    fi
+
+    # Create thumbnails.
+    for SIZE in $SIZES;do
+        cli_printMessage "${FILE}-thumb-${SIZE}.png" "AsSavedAsLine"
+        convert -resize ${SIZE} ${FILE}.png ${FILE}-thumb-${SIZE}.png
+        cli_printMessage "${FILE}-thumb-${SIZE}.jpg" "AsSavedAsLine"
+        convert -resize ${SIZE} ${FILE}-thumb-${SIZE}.png ${FILE}-thumb-${SIZE}.jpg
+        cli_printMessage "${FILE}-thumb-${SIZE}.pdf" "AsSavedAsLine"
+        convert -resize ${SIZE} ${FILE}-thumb-${SIZE}.png ${FILE}-thumb-${SIZE}.pdf
+    done
+
+}
