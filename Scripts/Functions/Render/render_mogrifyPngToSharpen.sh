86a1acc62f7b1051898afcf599af1da8158762cf
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-05-03 16:40:42 +0000

    Add render_mogrifyPngToSharpen.sh

---
+++ Scripts/Functions/Render/render_mogrifyPngToSharpen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# render_mogrifyPngToSharpen.sh -- This function provides
+# post-rendition action to sharpen PNG base-rendition output.
+#
+# Copyright (C) 2009, 2010, 2011 The CentOS Project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# ----------------------------------------------------------------------
+# $Id$
+# ----------------------------------------------------------------------
+
+function render_mogrifyPngToSharpen {
+
+    # Verify PNG base-rendition output.
+    cli_checkFiles "${FILE}.png"
+
+    # Execute mogrify action.
+    mogrify -sharpen "$FLAG_SHARPEN" ${FILE}.png
+
+}
