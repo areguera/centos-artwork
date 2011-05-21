83bfa6add4eb1565762ba25086755764e5d1cd3d
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-05-21 18:44:16 +0000

    Update render_svg_convertGplToHex.sh.

--- Scripts/Functions/Render/render_svg_convertGplToHex.sh
+++ Scripts/Functions/Render/render_svg_convertGplToHex.sh
@@ -51,7 +51,7 @@ function render_svg_convertGplToHex {
     fi
 
     # Define list of colors from GPL palette.
-    local COLORS=$(render_getColors "$PALETTE_GPL")
+    local COLORS=$(render_svg_getColors "$PALETTE_GPL")
 
     # Verify number of colors returned in the list.
     if [[ ! $(echo "$COLORS" |  wc -l) =~ $COLOR_NUMBER ]];then
