e20293631ea359ceb97d6a3ebd9734e8f5c62684
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-10 03:58:57 +0000

    Add docbook_doLastActions.sh.

---
+++ Scripts/Functions/Render/Backends/Docbook/docbook_doLastActions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# docbook_doLastActions.sh -- This function performs last-rendition
+# actions for DocBook files.
+#
+# Copyright (C) 2009, 2010, 2011 The CentOS Artwork SIG
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# ----------------------------------------------------------------------
+# $Id$
+# ----------------------------------------------------------------------
+
+function docbook_doLastActions {
+
+    # Presently, there is no last-rendition action for DocBook base
+    # rendition but the function should exist for consistency with
+    # other backends.
+    return
+
+}
