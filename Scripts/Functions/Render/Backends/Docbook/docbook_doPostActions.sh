53dad750614fe26c07a88c313921c1106215090b
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-11 18:47:08 +0000

    Update comments in docbook_doPostActions.sh.

--- Scripts/Functions/Render/Backends/Docbook/docbook_doPostActions.sh
+++ Scripts/Functions/Render/Backends/Docbook/docbook_doPostActions.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
 # docbook_doPostActions.sh -- This function performs post-rendition
-# actions for DocBook files.
+# actions for DocBook files. These are the actions that take
+# base-rendition output as input to producing output from it.
 #
 # Copyright (C) 2009, 2010, 2011 The CentOS Artwork SIG
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
