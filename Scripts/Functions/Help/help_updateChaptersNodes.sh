d2b3bf46ea9ca958fd1a63fec71d2bc3ac0c9885
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-12 02:52:48 +0000

    Update help_updateChaptersNodes.sh.

--- Scripts/Functions/Help/help_updateChaptersNodes.sh
+++ Scripts/Functions/Help/help_updateChaptersNodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# texinfo_updateChaptersNodes.sh -- This function updates nodes of
+# help_updateChaptersNodes.sh -- This function updates nodes of
 # chapters based on menu of chapters.
 #
 # Copyright (C) 2009, 2010, 2011 The CentOS Artwork SIG
@@ -23,7 +23,7 @@
 # $Id$
 # ----------------------------------------------------------------------
 
-function texinfo_updateChaptersNodes {
+function help_updateChaptersNodes {
 
     # Build list "nodes of chapters" based on menu of chapters.
     local CHAPTERNODES=$(cat ${MANUAL_BASEFILE}-menu.${MANUAL_EXTENSION} \
