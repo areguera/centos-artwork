2281d6632f160c2a31e2dffba17ca7e82b6868a4
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-12 02:53:09 +0000

    Update help_updateOutputFilePdf.sh.

--- Scripts/Functions/Help/help_updateOutputFilePdf.sh
+++ Scripts/Functions/Help/help_updateOutputFilePdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# texinfo_updateOutputFilePdf.sh -- This function exports documentation
+# help_updateOutputFilePdf.sh -- This function exports documentation
 # manual to PDF format.
 #
 # Copyright (C) 2009, 2010, 2011 The CentOS Artwork SIG
@@ -23,7 +23,7 @@
 # $Id$
 # ----------------------------------------------------------------------
 
-function texinfo_updateOutputFilePdf {
+function help_updateOutputFilePdf {
 
     # Output action message.
     cli_printMessage "${MANUAL_BASEFILE}.pdf" --as-updating-line
