c4fb9be0ca3d5f554bec8e7005cd777c30258a5c
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-12 02:52:49 +0000

    Update help_editEntry.sh.

--- Scripts/Functions/Help/help_editEntry.sh
+++ Scripts/Functions/Help/help_editEntry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# texinfo_editEntry.sh -- This function implements the edition flow of
+# help_editEntry.sh -- This function implements the edition flow of
 # documentation entries inside the working copy.
 #
 # Copyright (C) 2009, 2010, 2011 The CentOS Artwork SIG
@@ -23,7 +23,7 @@
 # $Id$
 # ----------------------------------------------------------------------
 
-function texinfo_editEntry {
+function help_editEntry {
 
     # Print separator line.
     cli_printMessage '-' --as-separator-line
