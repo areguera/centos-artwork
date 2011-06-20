9b64fcb5c596916854d33cc8f29be8cce2612c38
Alain Reguera Delgado <alain.reguera@gmail.com>
2011-06-20 03:46:00 +0000

    Update `render' functionality:
        - Move backend-specific functionalities out of Backends directory.
          There is no need to have an extra level of organization, here.

--- Scripts/Functions/Render/render.sh
+++ Scripts/Functions/Render/render.sh
@@ -72,7 +72,7 @@ function render {
 
     # Initialize absolute path to backend's base directory, the place
     # where backend-specific directories are stored in.
-    local RENDER_BACKEND_DIR="${FUNCDIR}/${FUNCDIRNAM}/Backends"
+    local RENDER_BACKEND_DIR="${FUNCDIR}/${FUNCDIRNAM}"
 
     # Initialize list of supported file extensions. These file
     # extensions are used by design model files, the files used as
