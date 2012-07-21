a96088e728af393c632795c840f5c436bed0b330
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-07-21 17:56:20 +0000

    Update Functions/Prepare/prepare_updateManuals.sh

--- Scripts/Bash/Functions/Prepare/prepare_updateManuals.sh
+++ Scripts/Bash/Functions/Prepare/prepare_updateManuals.sh
@@ -34,22 +34,22 @@ function prepare_updateManuals {
     # in the repository.
     local MANUALS=Manuals
 
-    # Define documentation manuals key names.
+    # Define key documentation manuals.
     local MANUALS_NAMES="Tcar-ug Tcar-fs"
 
-    # Define relative paths of design models and images used by
-    # documentation manuals.
-    local MANUALS_IMAGES=Identity/Images/Manuals
-    local MANUALS_MODELS=Identity/Models/Manuals
-
-    # Verify related design models and images used by documentation manuals.
-    for DIR in $(echo "$MANUALS_IMAGES $MANUALS_MODELS");do
+    # Verify related design models, images, and common stylesheets
+    # used by documentation manuals.
+    for DIR in $(echo "Identity/Images/Manuals
+                       Identity/Models/Manuals
+                       Identity/Webenv/Css
+                       Identity/Models/Webenv
+                       Identity/Images/Webenv");do
         MANUALS_ABSPATH=$(cli_getRepoTLDir)/${DIR}
         if [[ ! -d $MANUALS_ABSPATH ]];then
             cli_printMessage "`eval_gettext "The directory \\\"\\\$MANUALS_ABSPATH\\\" doesn't exist."`" 
             cli_printMessage "`gettext "Do you want to download a working copy for it now?"`" --as-yesornorequest-line
             mkdir -p $MANUALS_ABSPATH
-            svn co $(cli_printUrl --projects-artwork)trunk/${DIR} $MANUALS_ABSPATH
+            svn co $(cli_printUrl --projects-artwork)trunk/${DIR} ${MANUALS_ABSPATH}
         fi
     done
 
@@ -64,8 +64,9 @@ function prepare_updateManuals {
         fi
     done
 
-    # Build output files related to key documentation manuals.
-    ${CLI_BASEDIR}/${CLI_NAME}.sh render trunk/${MANUALS_IMAGES} --dont-commit-changes
+    # Render dependent files and key documentation manuals.
+    ${CLI_BASEDIR}/${CLI_NAME}.sh render trunk/Identity/Images/Manuals --dont-commit-changes
+    ${CLI_BASEDIR}/${CLI_NAME}.sh render trunk/Identity/Images/Webenv --dont-commit-changes
     ${CLI_BASEDIR}/${CLI_NAME}.sh render trunk/Manuals/Tcar-ug --filter="tcar-ug" --dont-commit-changes
     ${CLI_BASEDIR}/${CLI_NAME}.sh help   trunk/Manuals/Tcar-fs --update --dont-commit-changes
 
