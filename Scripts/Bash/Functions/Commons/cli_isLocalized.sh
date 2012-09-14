7952f70f7ca792ca93c46f9b0f6aca7a58b9b7dd
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-09-14 19:57:29 +0000

    Update `Functions/Commons/cli_isLocalized.sh' file.

--- Scripts/Bash/Functions/Commons/cli_isLocalized.sh
+++ Scripts/Bash/Functions/Commons/cli_isLocalized.sh
@@ -51,7 +51,7 @@ function cli_isLocalized {
     # locale-specific directories will be created for.
     DIRS[++((${#DIRS[*]}))]="$(cli_getRepoTLDir)/Identity/Models/Themes/[[:alnum:]-]+/(Distro/$(\
         cli_getPathComponent --release-pattern)/Anaconda|Concept|Posters|Media)"
-    DIRS[++((${#DIRS[*]}))]="$(cli_getRepoTLDir)/Documentation/(Models|Manuals)/Docbook/[[:alnum:]-]+$"
+    DIRS[++((${#DIRS[*]}))]="$(cli_getRepoTLDir)/Documentation/Models/Docbook/[[:alnum:]-]+$"
     DIRS[++((${#DIRS[*]}))]="$(cli_getRepoTLDir)/Scripts/Bash$"
 
     # Verify location passed as first argument agains the list of
