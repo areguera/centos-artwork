bfd78aeb85c4d4fd953d770e86dc3bdd262ad471
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-09-15 22:39:28 +0000

    Update `Functions/Render/render.sh' file.

--- Scripts/Bash/Functions/Render/render.sh
+++ Scripts/Bash/Functions/Render/render.sh
@@ -3,7 +3,7 @@
 # render.sh -- This function initializes rendition variables and
 # actions to centos-art.sh script.
 #
-# Copyright (C) 2009-2012 The CentOS Project
+# Copyright (C) 2009, 2010, 2011, 2012 The CentOS Project
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
