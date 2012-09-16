2f2639b487cefd768e856bde81cc2270ea37d6ee
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-09-16 20:14:38 +0000

    Update `Functions/Locale/locale_editMessages.sh' file.

--- Scripts/Bash/Functions/Locale/locale_editMessages.sh
+++ Scripts/Bash/Functions/Locale/locale_editMessages.sh
@@ -25,6 +25,13 @@
 
 function locale_editMessages {
 
+    # Verify current locale information to avoid English messages from
+    # being localized to themselves.  The English language is used as
+    # reference to write translatable strings inside the source files.
+    if [[ $(cli_getCurrentLocale) =~ '^en' ]];then
+        cli_printMessage "`gettext "The English language cannot be localized to itself."`" --as-error-line
+    fi
+
     local PO_FILE=''
     local PO_FILES=''
 
