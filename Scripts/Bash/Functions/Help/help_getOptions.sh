9992fc6a87157f7cf750e3fe0e27342e0bea5529
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-09-16 14:06:16 +0000

    Update `Functions/Help/help_getOptions.sh' file.

--- Scripts/Bash/Functions/Help/help_getOptions.sh
+++ Scripts/Bash/Functions/Help/help_getOptions.sh
@@ -30,7 +30,7 @@ function help_getOptions {
     local ARGSS=""
 
     # Define long options we want to support.
-    local ARGSL="quiet,answer-yes,dont-commit-changes,read,search:,edit,update-output,update-structure,copy,delete,rename"
+    local ARGSL="quiet,answer-yes,read,search:,edit,update-output,update-structure,copy,delete,rename"
 
     # Parse arguments using getopt(1) command parser.
     cli_parseArguments
@@ -45,17 +45,11 @@ function help_getOptions {
 
             --quiet )
                 FLAG_QUIET="true"
-                FLAG_DONT_COMMIT_CHANGES="true"
                 shift 1
                 ;;
 
             --answer-yes )
                 FLAG_ANSWER="true"
-                shift 1
-                ;;
-
-            --dont-commit-changes )
-                FLAG_DONT_COMMIT_CHANGES="true"
                 shift 1
                 ;;
 
