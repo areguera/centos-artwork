04d83d02434ba2503d315add2f89c21ffe313e5a
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-10-06 03:29:17 +0000

    Update `Functions/Locale/locale_getOptions.sh' file.

--- Scripts/Bash/Functions/Locale/locale_getOptions.sh
+++ Scripts/Bash/Functions/Locale/locale_getOptions.sh
@@ -27,10 +27,10 @@
 function locale_getOptions {
 
     # Define short options we want to support.
-    local ARGSS="v"
+    local ARGSS="q"
 
     # Define long options we want to support.
-    local ARGSL="verbose,filter:,answer-yes,update,edit,delete,dont-create-mo,is-localizable,sync-changes"
+    local ARGSL="quiet,filter:,answer-yes,update,edit,delete,dont-create-mo,is-localizable,sync-changes"
 
     # Redefine ARGUMENTS using getopt(1) command parser.
     cli_parseArguments
@@ -48,8 +48,8 @@ function locale_getOptions {
                 shift 2
                 ;;
 
-            -v | --verbose )
-                FLAG_VERBOSE="true"
+            -q | --quiet )
+                FLAG_QUIET="true"
                 shift 1
                 ;;
 
