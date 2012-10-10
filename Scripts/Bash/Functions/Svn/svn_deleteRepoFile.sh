b7dc9d911170eab4d6d361618da260f9a1ab3c27
Alain Reguera Delgado <alain.reguera@gmail.com>
2012-10-10 19:37:23 +0000

    Add `Functions/Svn/svn_deleteRepoFile.sh' file.

---
+++ Scripts/Bash/Functions/Svn/svn_deleteRepoFile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# svn_deleteRepoFile.sh -- This function standardizes the way
+# centos-art.sh script deletes files and directories inside the
+# working copy.
+#
+# Copyright (C) 2009, 2010, 2011, 2012 The CentOS Project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# ----------------------------------------------------------------------
+# $Id$
+# ----------------------------------------------------------------------
+
+function svn_deleteRepoFile {
+
+    local TARGET=$(cli_checkRepoDirSource ${1})
+
+    # Print action reference.
+    cli_printMessage "${TARGET}" --as-deleting-line
+
+    # Verify target existence.
+    cli_checkFiles -e ${TARGET}
+
+    # Revert changes before deleting related files.
+    ${SVN} revert ${TARGET} --quiet --recursive
+
+    # Delete source location.
+    ${SVN} delete ${TARGET} --quiet --force
+
+}
