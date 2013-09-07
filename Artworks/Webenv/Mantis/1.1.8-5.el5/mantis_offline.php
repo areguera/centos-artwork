47b64b92d075ac8c366beac662eb2a9e0ba1d8f5
Alain Reguera Delgado <al@centos.org.cu>
2013-09-07 14:32:04 -0400

    Reorganize directory structure of mantis-1.1.8-5.el5 theme.
    - Previously, the files of this customization were put directly inside
      /usr/share/mantis directory structure without any specific order
      except that specified by mantis' directory structure itself.  For
      example, images in this customization were copied into the images/
      directory, html files were put directly in mantis' root directory
      and stylesheets under css/ directory. This distribution is very
      difficult to maintain, specially if more than one customization is
      copied inside the mantis' root directory. This commit fixes the
      maintainability issue by creating a themes/ directory inside mantis'
      root directory where customizations will be all stored and organized
      by name.
    
    - This commit reorganizes file names, making them shorter. It also
      updates references inside HTML and CSS files to use the new names
      correctly.
    
    - The README file was updated to better describe the process through
      which this customization can be installed. It also provides an
      introduction describing what this customization is intended for.
    
    - This commit adds a customized version of mantis_offline.php file, too. So
      mantis application and its maintenance page share both the same visual
      style.
    
    - Standardize CSS writing style.

---
+++ Artworks/Webenv/Mantis/1.1.8-5.el5/mantis_offline.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html>
+  <head>
+    <title>Mantis</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Refresh" content="600" />
+    <link rel="stylesheet" type="text/css" href="themes/centos/stylesheet.css" />
+    <link rel="shortcut icon" href="themes/centos/images/favicon.ico" type="image/x-icon" />
+  </head>
+
+  <body>
+
+<div id="header">
+    <div id="logo">
+	<a title="The CentOS Bugs" href="/bugs/"><img src="themes/centos/images/logo.png" alt="The CentOS Bugs"></a>
+    </div>
+    <hr class="pageline blue" />
+</div>
+
+    <div id="box">
+
+      <h1>Off-line for maintenance!</h1>
+
+      <p>
+        The CentOS Bugs' tracker is currently off-line for maintenance.
+        Please check back later to see if it is online again or
+        contact your administrator for details.
+      </p>
+
+      <p>
+        Use your browser's refresh or reload function to try again
+        in a few minutes.
+      </p>
+
+    </div>
+
+<div id="footer">
+<hr class="pageline blue" />
+
+<div id="credits">
+    <p><a href="/bugs/">The CentOS Bugs</a></p>
+    <p>&copy; 2013 <a href="http://www.centos.org/">The CentOS Project</a></p>
+</div>
+
+</div>
+
+  </body>
+</html>
