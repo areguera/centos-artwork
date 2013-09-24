c9c442b76f367d0fd851b09cec58cb7ab3166e18
Alain Reguera Delgado <al@centos.org.cu>
2013-09-24 00:47:47 -0400

    Update copyright holder for prepare module and its sub-modules.

--- Automation/Modules/Prepare/prepare_setRenderEnvironment.sh
+++ Automation/Modules/Prepare/prepare_setRenderEnvironment.sh
@@ -22,7 +22,7 @@
 #   Written by:
 #   * Alain Reguera Delgado <al@centos.org.cu>, 2009-2013
 #
-# Copyright (C) 2009-2013 The CentOS Project
+# Copyright (C) 2009-2013 The CentOS Artwork SIG
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
