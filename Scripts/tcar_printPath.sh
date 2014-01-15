b6d3534460820e73ab3263eb6ef779cb7eb4c9a1
Alain Reguera Delgado <al@centos.org.cu>
2014-01-14 23:02:46 -0500

    Add tcar_printPath function.

---
+++ Scripts/tcar_printPath.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+######################################################################
+#
+#   tcar - The CentOS Artwork Repository automation tool.
+#   Copyright © 2014 The CentOS Artwork SIG
+#
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#   Alain Reguera Delgado <al@centos.org.cu>
+#   39 Street No. 4426 Cienfuegos, Cuba.
+#
+######################################################################
+
+function tcar_printPath {
+
+    local LOCATION=${1}
+
+    # Remove consecutive slashes, trailing slashes and any dot-slash
+    # or double-dot slash construction from final location.
+    if [[ ${LOCATION} ]];then
+        echo "${LOCATION}" | sed -r -e 's,/+,/,g' -e 's,/+$,,g' -e 's,\.+/,,g'
+    fi
+
+}
